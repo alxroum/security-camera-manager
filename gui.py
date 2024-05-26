18589df2c04294afcaa53571d725db8f66eb8b9f
Alex Ro <alxroum@gmail.com>
2024-05-26 14:26:24 -0400

    made ui changes.

--- gui.py
+++ gui.py
@@ -12,42 +12,41 @@ class GUI(Tk):
         self.width = width
         self.height = height
 
+        self.geometry("600x400")  # setting default size of the window
+
         # fixing window size for now
-        self.minsize(self.width, self.height)
-        self.maxsize(self.width, self.height)
+        # self.minsize(self.width, self.height)
+        # self.maxsize(self.width, self.height)
 
         # creating the main frame
-        self.main_frame = Frame(self)
+        self.main_frame = Frame(self, bg="#2b2b2e")  # main background frame
         self.main_frame.pack(expand="yes", fill="both")
 
-        # creating the button frame which is placed on the bottom
-        self.button_frame = Frame(self.main_frame, bg="#1a1a1c")
-        self.button_frame.pack(expand="yes", fill="x", side="bottom", anchor="s")
+        # creating the navigation bar frame which is placed at the top
+        self.nav_frame = Frame(self.main_frame, bg="#1a1a1c")
+        self.nav_frame.pack(expand="yes", fill="x", side="top", anchor="n")
 
-        # creating the buttons
-        self.start_rec = tk.Button(self.button_frame, text="Start Recording", bg="#47a123")
-        self.start_rec.pack(side="left", padx=15, pady=15)
-        self.new_frame = tk.Button(self.button_frame, text="Display New Frame")
-        self.new_frame.pack(side="left", fill="both", expand="yes", padx=20, pady=20)
-        self.stop_rec = tk.Button(self.button_frame, text="Stop Recording", bg="#bf2222")
-        self.stop_rec.pack(side="right", padx=15, pady=15)
+        self.side_bar = Frame(self.main_frame, bg="white")
+        self.side_bar.pack(expand="yes", fill="y", side="bottom", anchor="w")
 
-    def start_recording(self):
-        pass
 
-    def stop_recording(self):
-        pass
 
-    def display_new_frame(self):
-        pass
+        self.menu_image = PhotoImage(file="assets/hamburger_menu_button_white.png")
+        self.menu_button = tk.Button(self.nav_frame, image=self.menu_image, bg="#2b2b30")
+        self.menu_button.pack(side="left", padx=10, pady=10)
+        self.menu_button.bind('<Button-1>', lambda e: show_side_menu())  # opens expanded menu when button is clicked
+        self.menu_button.bind('<Leave>', lambda e: hide_side_menu())  # exits when mouse leaves expanded menu
 
 
 def main():
-    # graphics = GUI(1000, 600)
-    # graphics.mainloop()
-    pass
+    graphics = GUI(1000, 600)
+    graphics.mainloop()
 
 
+def show_side_menu():
+    print("showing side menu")
 
+def hide_side_menu():
+    print("hiding side menu")
 
 main()
