b9cde608b4d32932062b8d5b56ee4006f977a834
Alex Ro <alxroum@gmail.com>
2024-05-30 22:42:54 -0400

    made some significant backwards changes to the ui, but a better idea of the final outcome is being made. the ui will be more simplified and the main menu will include a dropdown select for each of the available cameras to view. then the ui will display the camera in a new window after an option is selected.

--- file_manager.py
+++ file_manager.py
@@ -1,26 +1,11 @@
 import gui
 import cv2
 
+# this program will take a video input from the video_capture file and save it to a new video in a specified folder
 
 def main():
-    print('Starting Program ...')
-    stream_video()
+    pass
 
-
-def stream_video():
-    url = 'http://192.168.0.110:8080/video'
-    cap = cv2.VideoCapture(url)
-
-    while True:
-        ret, frame = cap.read()
-        if frame is not None:
-            cv2.imshow("frame", frame)
-
-        q = cv2.waitKey(1)
-        if q == ord("q"):
-            break
-
-    cv2.destroyAllWindows
 
 
 
--- gui.py
+++ gui.py
@@ -1,36 +1,51 @@
 from tkinter import *
 import tkinter as tk
 import customtkinter
+from tkinter import ttk
 from tkinter_webcam import webcam
+import ttkthemes
 import cv2
 
 
 class GUI(Tk):
 
-    def __init__(self, width, height):
+    def __init__(self):
         Tk.__init__(self)
 
         color_1 = '#1a1a1c'
         color_2 = '#2b2b2e'
 
-        # variables
-        self.width = width
-        self.height = height
-
-        self.side_menu_open = False
-
         # pre display settings
         self.title('Security Camera Viewer')
         icon = PhotoImage(file='assets/camera_icon.png')
         self.iconphoto(True, icon)
-        self.geometry("600x400")  # setting default size of the window
+        self.minsize(width=300, height=150)
+        # self.geometry("600x400")  # setting default size of the window
+        self.configure(bg=color_2)
+
+        style = ttkthemes.ThemedStyle()
+        style.theme_use("equilux")
+
+
+
+        cams = ['default', 'Camera 1', 'Camera 2', 'Camera 3']
+        self.lab1 = Label(text='Select a Camera to View', bg=color_2, fg='white', font='Helvetica 10 bold')
+        self.lab1.pack()
+        self.cam_select = ttk.Combobox(self, values=cams)
+
+        self.cam_select.pack()
+        self.cam_select['state'] = 'readonly'
+        self.cam_select.set('default')
+        self.cam_select.bind(sequence='<<ComboboxSelected>>', func=lambda event: self.cam_change(event, self.cam_select))
+
+        """
 
         # creating the navigation bar frame which is placed at the top
         self.nav_frame = Frame(self, bg=color_1)
         self.nav_frame.pack(side='top', fill='x')
 
         self.menu_image = PhotoImage(file="assets/hamburger_menu_button_white.png")
-        self.menu_button = tk.Button(self.nav_frame, width=16, height=16, image=self.menu_image, bg=color_1, relief="solid", borderwidth=0, command=self.side_menu)
+        self.menu_button = tk.Button(self.nav_frame, width=16, height=16, image=self.menu_image, bg=color_1, relief="solid", borderwidth=0)
         self.menu_button.pack(side="left", padx=10, pady=10)
 
         # main frame holding the bottom section of the screen under the nav-bar
@@ -40,42 +55,15 @@ class GUI(Tk):
         # sidebar
         self.sidebar = Frame(self.main_frame, bg=color_1)
         self.sidebar.place(relx=0, rely=0, relwidth=0.2, relheight=1)
+        
+        """
 
-        # creating the main camera display frame
-        self.cam_frame = Frame(self.main_frame, bg=color_2)  # main background frame
-        self.cam_frame.place(relx=0.2, rely=0, relwidth=0.8, relheight=1)
-
-
-        self.cam_1 = Frame(self.cam_frame, bg='blue')
-        self.cam_1.grid(row=0, column=0, sticky="nsew", rowspan=2)
-        self.img1 = PhotoImage(file='assets/hamburger_menu_button.png')
-        self.label = Label(self.cam_1, image=self.img1)
-        self.label.pack()
-
-        self.cam_2 = Frame(self.cam_frame, bg='red')
-        self.cam_2.grid(row=0, column=1, sticky="nsew")
-
-        if self.side_menu_open:
-            print('open')
-
-
-    def side_menu(self):
-
-        if self.side_menu_open:
-            print("hiding side menu")
-            self.side_menu_open = False
-
-        if not self.side_menu_open:
-            print("showing side menu")
-            self.side_menu_open = True
-
-    def cam_change(self, e):
-        print(f'Camera Changed To {e}')
-
+    def cam_change(self, e, box):
+        print(f'Camera is Set To {box.get()}')
 
 
 def main():
-    graphics = GUI(1000, 600)
+    graphics = GUI()
     graphics.mainloop()
 
 
